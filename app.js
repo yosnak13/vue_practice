40b6733b073d76681d3f0124e99254001e9ea4ae
yosnak13 <yosnak13@gmail.com>
2021-05-14 08:25:44 +0900

    1000未満だとエラーメッセージを出力（ディレクティブ機能）

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ var vm = new Vue ({
     },
     totalPriceWithTax: function () {
       return Math.floor(this.totalPrice * 1.1)
+    },
+    canBuy: function () {
+      return this.totalPrice >= 1000 //1000円以上で購入可能
     }
   }
 })
